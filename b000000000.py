a2dc7b3c04222151b5df0cb51664cdb3fe29d5b9
Raissa111 <ernanjia@gmail.com>
2020-04-04 19:57:58 -0400

    La taille de la queue est <mon 4>

--- b000000000.py
+++ b000000000.py
@@ -3,9 +3,31 @@
 
 @author: Raissa111
 """
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
 
-def main():
-   print('Informatique: le rêve')
+from collections import deque
+def personne_elue(name):
+    return name == 'Zoureni'
+
+def search(name):
+   search_queue = deque()
+   search_queue += eleves[name]
+   print( len(search_queue) )
+   return False
 
 if __name__== "__main__":
-  main()
+   search("Boris")
